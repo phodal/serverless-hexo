ec421edb050e8f5dc418a012e9a181d054309660
Phodal Huang <h@phodal.com>
2017-11-01 15:00:11 +0800

    [T] refactor vars

--- handler.js
+++ handler.js
@@ -10,25 +10,29 @@ const S3 = new AWS.S3(require('./s3config.js')());
 const walk = require('walk');
 
 let create = (event, context, callback) => {
-  request.get('https://github.com/phodal/serverless-hexo-blog-static-files/archive/master.zip')
-    .pipe(fs.createWriteStream('/tmp/blog.zip'))
-    .on('close', (repo) => {
-      fs.createReadStream('/tmp/blog.zip')
+  let repoAddress = 'https://github.com/phodal/serverless-hexo-blog-static-files';
+  let blogZipFile = '/tmp/blog.zip';
+  let repoUnzipPath = "/tmp/serverless-hexo-blog-static-files-master";
+  let bucketName = 'static.wdsm.io';
+
+  request.get(`${repoAddress}/archive/master.zip`)
+    .pipe(fs.createWriteStream(blogZipFile))
+    .on('close', (repo) => {fs.createReadStream(blogZipFile)
         .pipe(unzip.Extract({path: '/tmp/'}))
         .on('close', function () {
           console.log(repo);
-          console.info(`Download https://github.com/phodal/serverless-hexo-blog-static-files`);
-          var hexo = new Hexo("/tmp/serverless-hexo-blog-static-files-master", {
-            config_path: '/tmp/serverless-hexo-blog-static-files-master/_config.yml'
+          console.info(`Download ${repoAddress}`);
+          var hexo = new Hexo(repoUnzipPath, {
+            config_path: `${repoUnzipPath}/_config.yml`
           });
 
-          hexo.loadPlugin(require.resolve('hexo-renderer-ejs'))
-          hexo.loadPlugin(require.resolve('hexo-renderer-marked'))
-          hexo.loadPlugin(require.resolve('hexo-renderer-stylus'))
-          hexo.loadPlugin(require.resolve('hexo-generator-tag'))
-          hexo.loadPlugin(require.resolve('hexo-generator-index'))
-          hexo.loadPlugin(require.resolve('hexo-generator-category'))
-          hexo.loadPlugin(require.resolve('hexo-generator-archive'))
+          hexo.loadPlugin(require.resolve('hexo-renderer-ejs'));
+          hexo.loadPlugin(require.resolve('hexo-renderer-marked'));
+          hexo.loadPlugin(require.resolve('hexo-renderer-stylus'));
+          hexo.loadPlugin(require.resolve('hexo-generator-tag'));
+          hexo.loadPlugin(require.resolve('hexo-generator-index'));
+          hexo.loadPlugin(require.resolve('hexo-generator-category'));
+          hexo.loadPlugin(require.resolve('hexo-generator-archive'));
 
           hexo.init().then(() => {
             console.info("Running Hexo Generate");
@@ -36,16 +40,16 @@ let create = (event, context, callback) => {
               .then(() => {
                 console.info(`Hexo done`);
                 // resolve full folder path
-                var walker  = walk.walk('/tmp/serverless-hexo-blog-static-files-master/public', { followLinks: false });
+                var walker  = walk.walk(`${repoUnzipPath}/public`, { followLinks: false });
                 walker.on('file', function(root, stat, next) {
-                  let filePath = root.toString().substring('/tmp/serverless-hexo-blog-static-files-master/public'.length + 1);
+                  let filePath = root.toString().substring(`${repoUnzipPath}/public`.length + 1);
                   let fileName = stat.name;
                   fs.readFile(path.join(root, stat.name), (error, fileContent) => {
                     if (error) {
                       throw error;
                     }
                     S3.putObject({
-                      Bucket: 'static.wdsm.io',
+                      Bucket: bucketName,
                       Key: path.join(filePath, fileName),
                       Body: fileContent
                     }, (res) => {
