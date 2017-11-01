f5d7551c30fcf0f4d8ef8cc59a9eef59634057ef
Phodal Huang <h@phodal.com>
2017-11-01 15:09:48 +0800

    [T] use es6 remplate for refactor

--- handler.js
+++ handler.js
@@ -11,35 +11,37 @@ const walk = require('walk');
 
 let create = (event, context, callback) => {
   let repoAddress = 'https://github.com/phodal/serverless-hexo-blog-static-files';
-  let blogZipFile = '/tmp/blog.zip';
-  let repoUnzipPath = "/tmp/serverless-hexo-blog-static-files-master";
+  let tmpPath = 'tmp/';
+  let blogZipFile = tmpPath + 'blog.zip';
+  let repoUnzipPath = `${tmpPath}/serverless-hexo-blog-static-files-master`;
   let bucketName = 'static.wdsm.io';
 
   request.get(`${repoAddress}/archive/master.zip`)
     .pipe(fs.createWriteStream(blogZipFile))
-    .on('close', (repo) => {fs.createReadStream(blogZipFile)
-        .pipe(unzip.Extract({path: '/tmp/'}))
+    .on('close', (repo) => {
+    fs.createReadStream(blogZipFile)
+        .pipe(unzip.Extract({path: tmpPath}))
         .on('close', function () {
-          console.log(repo);
-          console.info(`Download ${repoAddress}`);
+          console.info(`Downloaded ${repoAddress}`);
           var hexo = new Hexo(repoUnzipPath, {
             config_path: `${repoUnzipPath}/_config.yml`
           });
 
+          hexo.loadPlugin(require.resolve('hexo-generator-archive'));
+          hexo.loadPlugin(require.resolve('hexo-generator-category'));
+          hexo.loadPlugin(require.resolve('hexo-generator-index'));
+          hexo.loadPlugin(require.resolve('hexo-generator-tag'));
           hexo.loadPlugin(require.resolve('hexo-renderer-ejs'));
           hexo.loadPlugin(require.resolve('hexo-renderer-marked'));
           hexo.loadPlugin(require.resolve('hexo-renderer-stylus'));
-          hexo.loadPlugin(require.resolve('hexo-generator-tag'));
-          hexo.loadPlugin(require.resolve('hexo-generator-index'));
-          hexo.loadPlugin(require.resolve('hexo-generator-category'));
-          hexo.loadPlugin(require.resolve('hexo-generator-archive'));
 
           hexo.init().then(() => {
             console.info("Running Hexo Generate");
-            hexo.call("generate", {})
+            hexo.call("generate", {force: true})
               .then(() => {
                 console.info(`Hexo done`);
-                // resolve full folder path
+                hexo.exit();
+
                 var walker  = walk.walk(`${repoUnzipPath}/public`, { followLinks: false });
                 walker.on('file', function(root, stat, next) {
                   let filePath = root.toString().substring(`${repoUnzipPath}/public`.length + 1);
--- test.js
+++ test.js
@@ -5,14 +5,14 @@ var hexo = new Hexo(path.join(process.cwd(), 'tmp/serverless-hexo-blog-static-fi
   config_path: path.join(process.cwd(), 'tmp/serverless-hexo-blog-static-files-master/_config.yml')
 });
 
-hexo.loadPlugin(require.resolve('hexo-renderer-marked'))
-hexo.loadPlugin(require.resolve('hexo-renderer-stylus'))
-hexo.loadPlugin(require.resolve('hexo-generator-tag'))
-hexo.loadPlugin(require.resolve('hexo-generator-index'))
-hexo.loadPlugin(require.resolve('hexo-generator-category'))
-hexo.loadPlugin(require.resolve('hexo-generator-archive'))
+hexo.loadPlugin(require.resolve('hexo-generator-archive'));
+hexo.loadPlugin(require.resolve('hexo-generator-category'));
+hexo.loadPlugin(require.resolve('hexo-generator-index'));
+hexo.loadPlugin(require.resolve('hexo-generator-tag'));
+hexo.loadPlugin(require.resolve('hexo-renderer-ejs'));
+hexo.loadPlugin(require.resolve('hexo-renderer-marked'));
+hexo.loadPlugin(require.resolve('hexo-renderer-stylus'));
 
-console.log(path.join(process.cwd(), 'tmp/serverless-hexo-blog-static-files-master/_config.yml'));
 hexo.init().then( () => {
   console.info("Running Hexo Generate");
 	hexo.call("generate",{force: true})
